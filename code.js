14e46e4e29f5dc9bb5338b566332fdb667092a50
lilybrongo <144718640+lilybrongo@users.noreply.github.com>
2024-10-15 11:53:00 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,33 +1,3 @@
-function quicksort(array) {
-    if (array.length <= 1) {
-        return array
-    }
-    let top = 0;
-    let lo = 0;
-    let hi = arrary.length - 1;
-    let stack = [];
-
-    stack [top] = low;
-    stack[++top] = hi;
-
-    while (top >= 0) {
-        hi = stack[top--];
-        lo = stack[top--];
-
-        let pivot = partition(array, lo, hi);
-        if (pivot - 1 > lo) {
-            stack[++top] = lo;
-            stack[++top] = pivot - 1;
-        }
-
-        if (pivot + 1 < hi) {
-            stack[++top] = pivot + 1;
-            stack[++top] = hi;
-        }
-    }
-    return array;
-}
-
 //swap function
 function swap(array, a, b) {
     let temp = array[a];
@@ -46,4 +16,32 @@ function partition(array, lo, hi) {
     }
     swap(array, ++pivot, i);
     return pivot;
+}
+function quicksort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+    let top = 0;
+    let lo = 0;
+    let hi = arrary.length - 1;
+    let stack = [];
+
+    stack [top] = low;
+    stack[++top] = hi;
+
+    while (top >= 0) {
+        hi = stack[top--];
+        lo = stack[top--];
+
+        let pivot = partition(array, lo, hi);
+        if (pivot - 1 > lo) {
+            stack[++top] = lo;
+            stack[++top] = pivot - 1;
+        }
+        if (pivot + 1 < hi) {
+            stack[++top] = pivot + 1;
+            stack[++top] = hi;
+        }
+    }
+    return array;
 }
